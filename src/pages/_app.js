ff82d31f8e07161e25226a2f2275bc4e9a2181be
Lincoln Bergeson <lincoln.bergeson@gmail.com>
2024-05-23 11:17:06 -0500

    pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
 import '@/styles/globals.css'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Baptism Tracker</title>
+        <meta name="description" content="Baptism Tracker" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/logo.png" color="#FFFFFF" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Baptism Tracker" />
+        <meta property="og:description" content="Baptism Tracker" />
+        <meta property="og:site_name" content="Baptism Tracker" />
+        <meta property="og:url" content="https://baptism-tracker.vercel.app" />
+        <meta property="og:image" content="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
